30deedbcc3e60770205c3bce75f8201ce0865b3a
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-07-28 14:15:32 -0700

    format, part 7 is not working

--- controllers/places.js
+++ controllers/places.js
@@ -54,13 +54,13 @@ router.delete('/:id', (req, res) => {
 router.put('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-      res.render('error404')
+    res.render('error404')
   }
   else if (!places[id]) {
-      res.render('error404')
+    res.render('error404')
   }
   else {
-      res.redirect(`/places/${id}`)
+    res.redirect(`/places/${id}`)
   }
 })
 
