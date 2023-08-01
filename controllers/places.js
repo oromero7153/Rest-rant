c2031a9db20a8263258b5485c004784b24a19288
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-08-01 09:08:17 -0700

    getting close to finishing part 6

--- controllers/places.js
+++ controllers/places.js
@@ -31,11 +31,9 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id] })
+    res.render('places/show', { place: places[id], id })
   }
 });
-
-
 
 // GET /places
 router.get('/', (req, res) => {
@@ -67,20 +65,6 @@ router.get('/', (req, res) => {
   else {
     places.splice(id, 1)
     res.redirect('/places')
-  }
-})*/
-
-//EDIT
-/*router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.redirect(`/places/${id}`)
   }
 })*/
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,22 +1,30 @@
 const React = require('react')
 const Def = require('../default')
 
-function show (data) {
-    return (
-        <Def>
-          <main>
-            <h1>{ data.place.name }</h1>
-            <div>
-            <h3>Rating</h3>
-            <p>No rating</p>
-            </div>
-            <div>
-            <h3>Comments</h3>
-            <p>No Comments</p>
-            </div>
-          </main>
-        </Def>
-    )
+function show(data) {
+  return (
+    <Def>
+      <main>
+        <h1>{data.place.name}</h1>
+        <div>
+          <h3>Rating</h3>
+          <p>No rating</p>
+        </div>
+        <div>
+          <h3>Comments</h3>
+          <p>No Comments</p>
+        </div>
+        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+          Edit
+        </a>
+        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+          <button type="submit" className="btn btn-danger">
+            Delete
+          </button>
+        </form>
+      </main>
+    </Def>
+  )
 }
 
 module.exports = show
