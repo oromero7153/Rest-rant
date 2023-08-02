c40f67466a84698f114f9ffbf987fe5945e98e5d
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-08-02 07:48:40 -0700

    move delete function in places controller

--- controllers/places.js
+++ controllers/places.js
@@ -35,6 +35,21 @@ router.get('/:id', (req, res) => {
   }
 });
 
+// delete
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+});
+
 // GET /places
 router.get('/', (req, res) => {
   let places = [{
@@ -53,19 +68,5 @@ router.get('/', (req, res) => {
   res.render('places/index', { places })
 });
 
-// delete
-/*router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
-})*/
 
 module.exports = router
