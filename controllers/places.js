313ecd354e4c955ed24d05ee5534d4220725668e
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-08-04 08:35:16 -0700

    completed the steps to part 9 need to test

--- controllers/places.js
+++ controllers/places.js
@@ -16,11 +16,21 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   db.Place.create(req.body)
   .then(() => {
-    res.redirect('/places')
+      res.redirect('/places')
   })
   .catch(err => {
-    console.log(err)
-    req.render( 'error404' )
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+      }
+      console.log('Validation error message', message)
+      res.render('places/new', { message })
+  }
+  else {
+      res.render('error404')
+  }
   })
 });
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,27 +5,51 @@ function show(data) {
   return (
     <Def>
       <main>
-        <h1>{data.place.name}</h1>
-        <div>
-          <h3>Rating</h3>
-          <p>No rating</p>
+        <h1>Add a New Place</h1>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <h2>
+              Rating
+            </h2>
+            <h3 className="inactive">
+              Not Rated
+            </h3>
+            <br />
+            <h2>
+              Description
+            </h2>
+            <h3>
+              {data.place.showEstablished()}
+            </h3>
+            <h4>
+              Serving {data.place.cuisines}
+            </h4>
+            <br />
+            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+              Edit
+            </a>{` `}
+            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+          </div>
         </div>
-        <div>
-          <h3>Comments</h3>
-          <p>No Comments</p>
-        </div>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
+        <hr />
+        <h2>Comments</h2>
+        <h3 className="inactive">
+          No comments yet!
+        </h3>
       </main>
     </Def>
   )
 }
-
 module.exports = show
 
