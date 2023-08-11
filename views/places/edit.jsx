31762b407ce105f9f0496b70f78bbaabc3ab4fd4
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-08-11 06:41:07 -0700

    rest rant part 11 complete

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -17,6 +17,14 @@ function edit_form(data) {
                                 value={place.name}
                                 required />
                         </div>
+                        <div className="form-group col-sm-4">
+                            <label htmlFor="founded">Founded</label>
+                            <input className="form-control"
+                                id="founded"
+                                name="founded"
+                                value={data.place.founded}
+                            />
+                        </div>
                         <div className="form-group col-sm-6">
                             <label htmlFor="pic">Place Picture</label>
                             <input
@@ -59,7 +67,7 @@ function edit_form(data) {
                     <input
                         className='btn btn-primary'
                         type='submit'
-                        value='Add Place' />
+                        value='Update Place' />
                 </form>
             </main>
         </Def>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -13,6 +13,19 @@ function show(data) {
     </h3>
   )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐️'
+    }
+    rating = (
+      <h3>
+        {stars} stars
+      </h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
